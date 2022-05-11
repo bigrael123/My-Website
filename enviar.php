6608c954c6bd2085f7fc863079e07bef7c86187f
bigrael123 <bigrael-x@hotmail.com>
2022-05-11 02:25:34 -0300

    Master

---
+++ enviar.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST["Email"]) && !empty($_POST[Email])){
+$Topico = addslashes($_POST()['Topico'])
+$Email = addslashes($_POST()['Email'])
+$Mensagem = addslashes($_POST()['Mensagem'])
+}
+
+$to = "bigrael-x@live.com";
+$subject ="Meu Site ".$Topico"\n"
+$body = "Email: ".$Email "\n"
+        "Mensagem ".$Mensagem
+
+$header = "From:bigrael-x@hotmail.com"."\r\n"
+        ."Reply-To:".$Email."\e\n"
+        ."X=Mailer:PHP/".phpversion()
+
+
+if(mail($to,$subject,$body,$header)){
+    echo("Email enviado com successo!")
+}else{
+    echo("O email não foi enviado")
+}
+?>
